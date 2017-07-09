506aeb887097b8fd4d9883945af19115c4f7c7db
Jason Collins <the.oem@gmail.com>
2017-07-09 16:03:34 -0600

    Fix/add Card component tests.

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -2,15 +2,36 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Card from './Card';
 
+const peopleCardData = { data_type: 'person' };
+const planetCardData = { data_type: 'planet' };
+const vehicleCardData = { data_type: 'vehicle' };
+
 it('renders a container element', () => {
-  const dom = shallow(<Card />);
+  const dom = shallow(<Card cardData={ peopleCardData }/>);
 
   expect(dom.find('.card')).toHaveLength(1);
 });
 
-it('renders a CardHeader and a CardData component', () => {
-  const dom = shallow(<Card />);
+it('renders a CardHeader', () => {
+  const dom = shallow(<Card cardData={ peopleCardData }/>);
 
   expect(dom.find('CardHeader')).toHaveLength(1);
-  expect(dom.find('CardData')).toHaveLength(1);
+});
+
+it('renders a PeopleCardData component appropriately', () => {
+  const dom = shallow(<Card cardData={ peopleCardData }/>);
+
+  expect(dom.find('PeopleCardData')).toHaveLength(1);
+});
+
+it('renders a PlanetCardData component appropriately', () => {
+  const dom = shallow(<Card cardData={ planetCardData }/>);
+
+  expect(dom.find('PlanetCardData')).toHaveLength(1);
+});
+
+it('renders a VehicleCardData component appropriately', () => {
+  const dom = shallow(<Card cardData={ vehicleCardData }/>);
+
+  expect(dom.find('VehicleCardData')).toHaveLength(1);
 });

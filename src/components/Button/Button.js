b5f0ef099571fb8d8e7a75f609ecf8ad767c83d2
Jason Collins <the.oem@gmail.com>
2017-07-09 10:25:36 -0600

    Add styling to data select buttons. Clean up styling on Scroll.

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,9 +2,25 @@ import React from 'react';
 import './Button.css';
 
 const Button = ({ type, handleClick }) => {
+  let renderStyle = 'btn-data';
+  switch (type) {
+    case 'people':
+      renderStyle = 'btn-data btn-people';
+      break;
+    case 'planets':
+      renderStyle = 'btn-data btn-planets';
+      break;
+    case 'vehicles':
+      renderStyle = 'btn-data btn-vehicles';
+      break;
+    default:
+      break;
+  }
+
   return (
     <div className='btn-dataview'>
-      <button className='btn-data' onClick={() => handleClick(type)}> {type} </button>
+      <button className={renderStyle}
+        onClick={() => handleClick(type)}> {type.toUpperCase()} </button>
     </div>
   );
 };

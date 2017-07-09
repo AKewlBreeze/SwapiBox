e6615f32ecd4d676a453b102e8de9e48c2ca2e2a
Jason Collins <the.oem@gmail.com>
2017-07-09 14:22:34 -0600

    Fix DataView tests.

--- src/components/DataView/DataView.test.js
+++ src/components/DataView/DataView.test.js
@@ -2,8 +2,28 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import DataView from './DataView';
 
+const currentData = {
+  results: [
+    { name: 'Luke' },
+    { name: 'Anakin' },
+    { name: 'Chewbacca' },
+    { name: 'Darth Vader' },
+    { name: 'Han Solo' },
+  ],
+};
+
 it('renders a container element', () => {
   const dom = shallow(<DataView />);
 
   expect(dom.find('.card-container')).toHaveLength(1);
+});
+
+it('renders 5 cards if 5 cards have been passed in', () => {
+  const dom = shallow(
+    <DataView
+      currentData={ currentData }/>,
+  );
+
+  const cards = dom.find('Card');
+  expect(cards.length).toEqual(5);
 });

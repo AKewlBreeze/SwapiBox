15c1031ecbcb4b06ee806741a6abf97df77dd4eb
Jason Collins <the.oem@gmail.com>
2017-07-09 15:09:40 -0600

    Fix CardHeader tests.

--- src/components/CardHeader/CardHeader.test.js
+++ src/components/CardHeader/CardHeader.test.js
@@ -5,6 +5,7 @@ import CardHeader from './CardHeader';
 const cardData = { name: 'Luke' };
 const favorites1 = [{ name: 'Luke' }];
 const favorites2 = [{ name: 'Han Solo' }];
+const mockedFn = jest.fn();
 
 it('renders a container element', () => {
   const dom = shallow(
@@ -16,24 +17,41 @@ it('renders a container element', () => {
   expect(dom.find('.card-header')).toHaveLength(1);
 });
 
-it('renders a button', () => {
-  const dom = shallow(
-    <CardHeader
-      cardData={ cardData }
-      favorites={ favorites1 }/>,
-  );
-
-  const button = dom.find('.btn-add-favorite');
-  expect(button).toHaveLength(1);
-});
-
 it('renders a Favorite button if the card is not favorited', () => {
   const dom = shallow(
     <CardHeader
       cardData={ cardData }
-      favorites={ favorites1 }/>,
+      favorites={ favorites1 }
+      handleFavorite={ mockedFn }/>,
+  );
+
+  expect(dom.find('.btn-add-favorite')).toHaveLength(1);
+  expect(dom.find('.btn-add-favorite').text()).toEqual(' unfavorite ');
+});
+
+it('renders a Unfavorite button if the card is favorited', () => {
+  const dom = shallow(
+    <CardHeader
+      cardData={ cardData }
+      favorites={ favorites2 }
+      handleFavorite={ mockedFn }/>,
+  );
+
+  expect(dom.find('.btn-add-favorite')).toHaveLength(1);
+  expect(dom.find('.btn-add-favorite').text()).toEqual(' favorite ');
+});
+
+it('has a favorite/unfavorite button that can be clicked', () => {
+  const dom = shallow(
+    <CardHeader
+      cardData={ cardData }
+      favorites={ favorites2 }
+      handleFavorite={ mockedFn }/>,
   );
 
   const button = dom.find('.btn-add-favorite');
-  expect(button).toHaveLength(1);
+  expect(dom.find('.btn-add-favorite')).toHaveLength(1);
+  button.simulate('click');
+  expect(mockedFn).toHaveBeenCalledTimes(1);
+  expect(mockedFn).toHaveBeenCalledWith(cardData);
 });

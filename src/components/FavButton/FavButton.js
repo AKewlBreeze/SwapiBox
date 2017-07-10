bf1508b2b866a9f27444912b7d77b986c4febe53
Jason Collins <the.oem@gmail.com>
2017-07-10 07:15:36 -0600

    Add additional styling for the card favorite buttons.

--- src/components/FavButton/FavButton.js
+++ src/components/FavButton/FavButton.js
@@ -4,7 +4,7 @@ import './FavButton.css';
 const FavButton = ({ favoritesCount, type, handleClick }) => {
   return (
     <div>
-      <button className='btn-favorite' onClick={() => handleClick(type)}>
+      <button className='btn-favorite-view' onClick={() => handleClick(type)}>
         Favorites: {favoritesCount}
       </button>
     </div>
